02488181a3cc9bba0fcacff9f7e6dedbb350baaa
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2022-11-12 20:01:07 -0300

    changing letters

--- python/7kyu/changing_letters.py
+++ python/7kyu/changing_letters.py
@@ -1,5 +1,5 @@
 # Link: https://www.codewars.com/kata/5831c204a31721e2ae000294/train/python
-# 
+#
 # Description:
 # When provided with a String, capitalize all vowels
 # For example:
@@ -20,3 +20,6 @@ def swap(st):
 
 
 print(swap('victor kolis'))
+
+# maketrans
+# translate
